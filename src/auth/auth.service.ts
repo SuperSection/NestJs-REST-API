a315e0214985e1f07fe680e7d67031f74773aef1
SuperSection <soumosarkar297@gmail.com>
2024-05-31 00:07:51 +0530

    setup prisma, add User and Bookmark models, Prisma module and implement auth DTO, nestjs global pipe & hash password with argon

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'I have signed up' };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // generate the hashed password
+    const hashedPassword = await argon.hash(dto.password);
+    // save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hashedPassword,
+      },
+    });
+
+    delete user.password;
+
+    // return the saved user
+    return user;
   }
 
   signin() {
